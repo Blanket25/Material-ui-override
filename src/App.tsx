40c97bdb01a203ac429f6d1f0ba981db3395ed91
Blanket25 <bianca_inga@hotmail.com>
2022-01-16 18:14:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  components: {
+     // Name of the component
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+           // CSS
+          backgroundColor: "#e1f5fe"
+        }
+      }
+    },
+     // Name of the component
+    MuiInputLabel:{
+      styleOverrides: {
+        root: {
+           // CSS
+          color: "#311b92"
+        }
+      }
+    },
+    // Name of the component
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          // CSS
+          backgroundColor: "#e1f5fe",
+          color: "#000063",
+          borderRadius: "5px"
+        },
+      },
+      //props
+      defaultProps: {
+        disableUnderline: true
+      }
+    },
+  },
+});
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
